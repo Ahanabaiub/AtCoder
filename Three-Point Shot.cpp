2ddbd3c28c324f83ed8aa79c88abd816c43f2c48
Ahanabaiub <54981138+Ahanabaiub@users.noreply.github.com>
2021-01-19 21:06:33 +0600

    Create Three-Point Shot.cpp

---
+++ Three-Point Shot.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define    fios ios_base::sync_with_stdio(false); cin.tie(0);
+#define    sf(x) scanf("%d",&x)
+#define    sfl(x) scanf("%lld",&x)
+#define    pf(x) printf("%d\n",x)
+#define    pfl(x) printf("%lld\n",x)
+#define    ll long long
+#define    nl "\n"
+#define    PB(x) push_back(x)
+#define    For(i,n) for(int i=0; i<n; i++)
+#define    PI acos(-1)
+using namespace std;
+
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+
+    if(abs(x-y)<3)
+    {
+        cout<<"Yes"<<nl;
+
+    }
+    else
+        cout<<"No"<<nl;
+
+
+    return 0;
+}
